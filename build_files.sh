39db1480fb3e8812c4b92302fb92c37df4d60733
DevelopersCbbiShuats34 <cbbishuats.developers@gmail.com>
2024-11-15 20:41:06 +0530

    Update build_files.sh 4

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,12 @@
 echo "BUILD START"
 
+/vercel/path0/venv/bin/python3.9 -m pip install --upgrade pip
 # create a virtual environment named 'venv' if it doesn't already exist
 python3.9 -m venv venv
 
 # activate the virtual environment
 source venv/bin/activate
+
 
 # install all deps in the venv
 pip install -r requirement.txt
