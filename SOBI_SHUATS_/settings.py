d7946208d6e3b82f4087a3f5f31e16e7393ff92b
DevelopersCbbiShuats34 <cbbishuats.developers@gmail.com>
2024-11-15 19:28:53 +0530

    Update settings.py

--- SOBI_SHUATS_/settings.py
+++ SOBI_SHUATS_/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY='django-insecure-3k_9xei=4qv5_!q7%z=yimd4dk%k=&9x!jyh!gc+^8mn4js7=4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", "127.0.0.1", ".now.sh"]
+ALLOWED_HOSTS = ["sbbs-mocha.vercel.app", "127.0.0.1", ".now.sh"]
 
 
 
